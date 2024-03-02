4d7cff531a4f3e93afd0ca152c3e0fe8e42e01ef
tapadar13 <monsurismyname@gmail.com>
2024-03-02 10:27:51 +0530

    feat: Implement 'getSketches' query and display recent sketches
    Introduce the 'getSketches' query to fetch recent sketches from the 'sketches' table. The fetched sketches are then displayed in the 'Recent Sketches' section of the component using the 'useQuery' hook and rendering images with associated details.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
 "use client";
 
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { api } from "@/convex/_generated/api";
+import { useMutation, useQuery } from "convex/react";
+import Image from "next/image";
+import image from "next/image";
 import { useForm } from "react-hook-form";
 
 export default function Home() {
+  const saveSketchMutation = useMutation(api.sketches.saveSketch);
+  const sketchesQuery = useQuery(api.sketches.getSketches);
+
   const {
     register,
     handleSubmit,
@@ -17,13 +26,29 @@ export default function Home() {
         <form
           className="flex flex-col gap-2 w-1/4"
           onSubmit={handleSubmit(async (formData) => {
-            console.log(formData);
+            await saveSketchMutation({ ...formData, image });
           })}
         >
-          <input {...register("prompt", { required: true })} />
+          <Label htmlFor="prompt">Prompt</Label>
+          <Input id="prompt" {...register("prompt", { required: true })} />
           {errors.prompt && <span>This field is required</span>}
           <input type="submit" />
         </form>
+
+        <section>
+          <h2>Recent Sketches</h2>
+          <div className="grid grid-cols-4 gap-4">
+            {sketchesQuery?.map((sketch) => (
+              <Image
+                key={sketch._id}
+                width="256"
+                height="256"
+                src={sketch.result}
+                alt="sketch"
+              />
+            ))}
+          </div>
+        </section>
       </div>
     </main>
   );
--- convex/sketches.ts
+++ convex/sketches.ts
@@ -1,13 +1,20 @@
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 import { v } from "convex/values";
 
 export const saveSketch = mutation({
   args: { prompt: v.string(), image: v.string() },
-  handler: async (ctx, { prompt }) => {
+  handler: async (ctx, { prompt, image }) => {
     const sketch = await ctx.db.insert("sketches", {
       prompt,
     });
 
     return sketch;
+  },
+});
+
+export const getSketches = query({
+  handler: async (ctx) => {
+    const sketches = await ctx.db.query("sketches").collect();
+    return sketches;
   },
 });
